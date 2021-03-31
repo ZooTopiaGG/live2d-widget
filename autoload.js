208b486facd9265977b9af204400918c067fe07a
dengpeng <zootopiagg@gmail.com>
2021-03-31 14:54:44 +0800

    feat: 吉祥物数据埋点

--- autoload.js
+++ autoload.js
@@ -30,10 +30,8 @@ if (screen.width >= 768) {
     loadExternalResource(live2d_path + "waifu.css", "css"),
     loadExternalResource(live2d_path + "live2d.min.js", "js"),
     loadExternalResource(live2d_path + "sha1.js", "js"),
-    loadExternalResource(live2d_path + "fetch.js", "js"),
     loadExternalResource(live2d_path + "waifu-tips.js", "js"),
   ]).then(() => {
-    console.log(sha1, request);
     initWidget({
       waifuPath: live2d_path + "waifu-tips.json",
       //apiPath: "https://live2d.fghrsh.net/api/",
